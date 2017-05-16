11815038f6dd66cc9fb62be23d046ff61475ee56
Jonathan Do <jonathaninikido@gmail.com>
2017-05-16 15:06:46 -0700

    Nightwatch e2e tests.

--- client/test/e2e/nwt1.js
+++ client/test/e2e/nwt1.js
@@ -1,13 +1,24 @@
 module.exports = {
-  'Demo test Google' : function (browser) {
+  'Sign-Up e2e Test WST' : function (browser) {
     browser
-      .url('http://www.google.com')
+      .url('https://lucky13-dev.herokuapp.com/')
       .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
+      .click("a[href='signup.html']")
+      .waitForElementVisible('#sign-up-body', 1000)
+			.setValue('#form-company-name', 'UCSD CSE112')
+			.setValue('#form-email', 'cse112@ucsd.edu');
+			.setValue('#form-phone', '14089111111');
+			.click('#submit-company-btn')
+			.waitForElementVisible('#form-employee-first', 1000);
+			.setValue('#form-employee-first', 'Thomas');
+			.setValue('#form-employee-last', 'Powell');
+			.setValue('#form-employee-email', 'cse112@ucsd.edu');
+			.setValue('#form-employee-phone', '14089111111');
+			.setValue('#form-password', 'cse112');
+			.setValue('#form-repeat-password', 'cse112');
+			.click('#submit-btn')
       .pause(1000)
-      .assert.containsText('#main', 'Night Watch')
+      .waitForElementVisible('.page-body gray loaded', 1000);
       .end();
   }
 };
