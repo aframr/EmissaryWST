678e0a330818954231a956696b4b853de68c50bb
Jonathan Do <jonathaninikido@gmail.com>
2017-06-01 16:12:27 -0700

    Nightwatch e2e test.

--- client/test/e2e/nwt1.js
+++ client/test/e2e/nwt1.js
@@ -2,14 +2,13 @@ module.exports = {
   'Login e2e Test Emissary' : function (browser) {
     browser
       .url('https://lucky13-dev.herokuapp.com/')
-      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('body', 5000)
       .click("a[href='login.html']")
-      .waitForElementVisible('#username', 1000)
+      .waitForElementVisible('#username', 5000)
 			.setValue('#username', 'ucsd@ucsd.edu')
 			.setValue('#password', 'ucsd')
 			.click('#loginButton')
-			.waitForElementVisible('#visitor-queue', 1000)
-      .pause(1000)
+			.waitForElementVisible('#visitor-queue', 5000)
       .end();
   }
 };
