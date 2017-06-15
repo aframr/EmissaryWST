8d91e72fde5dfa21d7fca2bacf5ab95ba8096c7f
Andrew Chan <chan94306@gmail.com>
2017-06-15 15:22:47 -0700

    ignore apidoc and gulp

--- client/assets/native/js/site.js
+++ client/assets/native/js/site.js
@@ -1,8 +1,8 @@
-/*navigator.serviceWorker && navigator.serviceWorker.register('assets/native/js/sw.js').then(function() {
+/* navigator.serviceWorker && navigator.serviceWorker.register('assets/native/js/sw.js').then(function() {
   console.log('Service Worker Registered');
 });*/
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('sw.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(() => { console.log('Service Worker Registered'); });
+}
