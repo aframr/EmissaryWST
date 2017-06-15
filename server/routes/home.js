dcd09854dcd58582a064cd2fe0ce594228dd7f68
Clarence Nguyen <clarrynguyen@gmail.com>
2017-06-14 17:25:42 -0700

    Adding webhook integration with api.ai

--- server/routes/home.js
+++ server/routes/home.js
@@ -6,6 +6,8 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
 
 /**
  * GET /
@@ -14,5 +16,86 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/visitors.html'));
 });
+
+/**
+ * webhook POST from api.ai
+ */
+router.post('/hook', function (req, res) {
+
+    console.log('hook request');
+
+    try {
+        var speech = 'empty speech';
+
+        if (req.body) {
+            var requestBody = req.body;
+
+            if (requestBody.result) {
+                speech = '';
+                
+                if (requestBody.result.action && requestBody.result.action == 'reservation')
+                {
+                    speech = handleReservation(requestBody);
+                }
+                else 
+                {
+                    // handle error case 
+                }
+            }
+        }
+
+        console.log('result: ', speech);
+
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'apiai-webhook-sample'
+        });
+    } catch (err) {
+        console.error("Can't process request", err);
+
+        return res.status(400).json({
+            status: {
+                code: 400,
+                errorType: err.message
+            }
+        });
+    }
+});
+
+
+// handle adding reservation to db 
+// 1. Check if reservation is edit/view/create/delete
+// 1.a case : edit 
+// 1.b case : view
+// 1.c case : create 
+// 1.d case : delete 
+function handleReservation(request) {
+    params = request.result.parameters;
+    response = "";
+    if (params.action == 'edit') {
+        // search database for item
+        // edit that item with given parameters 
+    }
+    else if (params.action == 'view') {
+        // search database for name/phone/email prompt 
+        // return it in response
+
+    }
+    else if (params.action == 'create') {
+        // create new entry in database with given parameters 
+
+    }
+    else if (params.action == 'delete') {
+        // search database for name,phone,email identification 
+        // delete the item from database 
+    }
+    else {
+        response += "Action not specified, please include action as 'edit','view','create', or 'delete'";
+        // action not specified 
+    }
+    return response 
+
+}
 
 module.exports = router;
